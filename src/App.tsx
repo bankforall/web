2b3fb039eb4fb5bbd08b0da22696fac3ae236594
Kittipod Lambangchang <tonkittipod@gmail.com>
2023-03-15 23:39:19 +0700

    feat(init): setup project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "@/pages/Home";
+
+const App: FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Home: FC = () => {
+  return <div>Bank For All</div>;
+};
+
+export default Home;
