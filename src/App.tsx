7b83ab0758e0e60c5a8ebe643406001d86f214b7
1tpp <panapat.dev@gmail.com>
2023-03-21 22:45:10 +0700

    feat: mockup ui peer-share room

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "@/pages/Home";
+import PeerShareRoom from "./pages/PeerShareRoom";
 
 const App: FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/peer-share-room" element={<PeerShareRoom />} />
     </Routes>
   );
 };
