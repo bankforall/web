6c642e805c326a4ececf78813f5f6e29afc3b30c
mmenuu <rctkhtp@gmail.com>
2023-03-25 20:31:54 +0700

    feat(wonBidding) : add won bidding screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Welcome from "@/screens/Welcome"
 import Dashboard from "@/screens/Dashboard"
 import PeerShareRoom from "@/screens/PeerShareRoom"
+import WonBidding from "@/screens/WonBidding"
 import Login from "@/screens/Login"
 import Register from "@/screens/Register"
 import PeerSharingDashboard from "@/screens/peerSharingDashboard";
@@ -14,7 +15,8 @@ const router = createBrowserRouter([
   { path: "/register", element: <Register /> },
   { path: "/peershare-room", element: <PeerShareRoom /> },
   { path: "/dashboard", element: <Dashboard />, },
-  { path: "/peershare-dashboard", element: <PeerSharingDashboard />}
+  { path: "/peershare-dashboard", element: <PeerSharingDashboard />},
+  { path: "/won-bidding", element: <WonBidding />}
 ])
 
 const App = () => {
