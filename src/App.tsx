2d0b0382e7dab81568b31555d424a27409afa9cb
1tpp <panapat.dev@gmail.com>
2023-03-25 07:24:05 +0700

    feat: welcome screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
-import Home from "@/screens/Home"
+import Welcome from "@/screens/Welcome"
 import Dashboard from "@/screens/Dashboard"
 import PeerShareRoom from "@/screens/PeerShareRoom"
 import Login from "@/screens/Login"
 import Register from "@/screens/Register"
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
+  { path: "/", element: <Welcome /> },
   { path: "/login", element: <Login /> },
   { path: "/register", element: <Register /> },
   { path: "/peershare-room", element: <PeerShareRoom /> },
